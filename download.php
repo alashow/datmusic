e3327e63c9e6972ab10bc3b37235fa67e3236dd3
Alashov Berkeli <yunus.alashow@gmail.com>
2015-04-25 14:56:07 +0500

    cyryllic filename to latin, better than spaces

--- download.php
+++ download.php
@@ -18,6 +18,18 @@ function makeSafe($file) {
     return trim(preg_replace($regex, '', $file));
 }
 
+function transliterate($textcyr = null, $textlat = null) {
+    $cyr = array(
+    'ж',  'ч',  'щ',   'ш',  'ю',  'а', 'б', 'в', 'г', 'д', 'e', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ъ', 'ь', 'я',
+    'Ж',  'Ч',  'Щ',   'Ш',  'Ю',  'А', 'Б', 'В', 'Г', 'Д', 'Е', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ъ', 'Ь', 'Я');
+    $lat = array(
+    'zh', 'ch', 'sht', 'sh', 'yu', 'a', 'b', 'v', 'g', 'd', 'e', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'y', 'x', 'q',
+    'Zh', 'Ch', 'Sht', 'Sh', 'Yu', 'A', 'B', 'V', 'G', 'D', 'E', 'Z', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'c', 'Y', 'X', 'Q');
+    if($textcyr) return str_replace($cyr, $lat, $textcyr);
+    else if($textlat) return str_replace($lat, $cyr, $textlat);
+    else return null;
+}
+
 function downloadFile ($url, $path) {
         if (isValidUrl($url)) {
           touch($path); 
@@ -64,7 +76,7 @@ function downloadFile ($url, $path) {
         exit();
  }
  $audio = $json['response'][0];
- $filename = makeSafe($audio["artist"] . " - " .  $audio["title"] . ".mp3");
+ $filename = makeSafe(transliterate($audio["artist"] . " - " .  $audio["title"] . ".mp3"));
  $audioUrl  = $audio["url"];
  $fullpath = "dl/" . $filename;
 
