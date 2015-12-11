5b977ff992cb23383998e1338d385411559e1f60
Alashov Berkeli <yunus.alashow@gmail.com>
2015-12-11 17:24:19 -0600

    bug fix

--- search.php
+++ search.php
@@ -8,9 +8,10 @@ include 'log.php';
 writeSearch($_GET['q']);
 
 $apiUrl = "https://api.vk.com/method/audio.search?";
-$_GET['access_token'] = "fff9ef502df4bb10d9bf50dcd62170a24c69e98e4d847d9798d63dacf474b674f9a512b2b3f7e8ebf1d69"; //for private access token. if public, comment this line so access token from js will be used
+$params = $_GET;
+$params['access_token'] = "fff9ef502df4bb10d9bf50dcd62170a24c69e98e4d847d9798d63dacf474b674f9a512b2b3f7e8ebf1d69"; //for private access token. if public, comment this line so access token from js will be used
 
-$fullUrl = $apiUrl . $_SERVER['QUERY_STRING'];
+$fullUrl = $apiUrl . http_build_query($params);
 
 print(file_get_contents($fullUrl));
 exit;
